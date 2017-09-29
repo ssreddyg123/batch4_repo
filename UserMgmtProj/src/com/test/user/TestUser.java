fa5f2ad94fa615b0e42d4891428db2937ce83b25
yhnreddy1 <yhnreddy@Toshiba>
2017-09-29 18:54:20 +0530

    Modified some code changes related to user

---
+++ UserMgmtProj/src/com/test/user/TestUser.java
@@ -0,0 +1,9 @@
+package com.test.user;
+
+public class TestUser {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- UserMgmtProj/src/com/test/user/User.java
+++ UserMgmtProj/src/com/test/user/User.java
@@ -1,5 +1,22 @@
 package com.test.user;
 
 public class User {
+	
+	private String userName;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 
 }
