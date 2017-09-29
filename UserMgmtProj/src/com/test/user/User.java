6a618b6ed06eef2046cc6a51e37292b0a193b124
yhnreddy1 <yhnreddy@Toshiba>
2017-09-29 18:56:36 +0530

    Removed password field

--- UserMgmtProj/src/com/test/user/User.java
+++ UserMgmtProj/src/com/test/user/User.java
@@ -3,7 +3,6 @@ package com.test.user;
 public class User {
 	
 	private String userName;
-	private String password;
 	
 	public String getUserName() {
 		return userName;
@@ -11,12 +10,7 @@ public class User {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 	
 
 }
